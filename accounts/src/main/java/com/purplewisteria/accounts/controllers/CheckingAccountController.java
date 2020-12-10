735088c2216c34df8f2b3f785a0acfeeb5ed985e
NiranjanaRao-P <niran21@gmail.com>
2020-12-10 08:03:18 -0500

    Checking Controller

--- accounts/src/main/java/com/purplewisteria/accounts/controllers/CheckingAccountController.java
+++ accounts/src/main/java/com/purplewisteria/accounts/controllers/CheckingAccountController.java
@@ -1,5 +1,7 @@
 package com.purplewisteria.accounts.controllers;
 
+//Controller
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
